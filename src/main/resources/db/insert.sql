6e81efd8e33d749c885c2c180682dd753b52f93a
Andrey Masterenko <masterenko.as@gmail.com>
2021-05-29 09:00:17 +0300

    0. Spring Liquibase [#302330]

---
+++ src/main/resources/db/insert.sql
@@ -0,0 +1,7 @@
+/*
+user's password = 123
+*/
+insert into users (username, password) values ('user', '$2a$10$CIOtBEoDodsLTff9psABW.yvXCF1JXwADvXUqFUCILy/g6sguv.OW');
+
+insert into posts (name, description) values ('What is this forum about?','bla-bla-bla');
+insert into posts (name, description) values ('The rules','bla-bla-bla');
--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -1,5 +1,3 @@
-create database forum;
-
 create table posts (
                        id serial primary key,
                        name varchar(2000),
@@ -12,11 +10,5 @@ create table users (
                        username VARCHAR(50) NOT NULL unique,
                        password VARCHAR(100) NOT NULL
 );
-/*
-user's password = 123
-*/
-insert into users (username, password) values ('user', '$2a$10$CIOtBEoDodsLTff9psABW.yvXCF1JXwADvXUqFUCILy/g6sguv.OW');
 
-insert into posts (name, description) values ('What is this forum about?','bla-bla-bla');
-insert into posts (name, description) values ('The rules','bla-bla-bla');
 
